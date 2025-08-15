6f8ae524fd33fc9dcfe96b122d2ffcdc9e3fa19c
Amanawl <amanawlachew775@gmain.com>
2025-08-15 14:34:43 +0300

    modifiy   List <Student> students = [];

--- Student_Management_System/main class.dart
+++ Student_Management_System/main class.dart
@@ -1,5 +1,25 @@
 void main (){
 
+
+Student student1 = Student("Abraham", 0101,[90.1,78.2]); 
+Student student2 = Student("Alina", 0201,[90.1,78.2]); 
+Student student3 = Student("yoln", 0301,[90.1,70.2]); 
+
+student1.addgrade(89.2);
+
+
+student3.addgrade(79);
+student2.addgrade(100);
+
+
+  List <Student> students = [];
+   students.add(student1);
+   students.add(student2);
+   students.add(student3);
+   
+for (var student in students){
+  student.display();
+}
 }
 
 class Student {
